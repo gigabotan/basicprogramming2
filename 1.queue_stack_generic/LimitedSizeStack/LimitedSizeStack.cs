96c06bbbbfdcfbca17d4f0da6e7369536307d069
gigabotan <aaazat7@gmail.com>
2026-02-19 02:19:38 +0100

    feat: add limitedsizestack solution

---
+++ 1.queue_stack_generic/LimitedSizeStack/LimitedSizeStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class LimitedSizeStack<T>
+{
+    private readonly int maxSize;
+    private readonly LinkedList<T> items = new();
+
+    public LimitedSizeStack(int undoLimit)
+    {
+        maxSize = undoLimit;
+    }
+
+    public void Push(T item)
+    {
+        if (maxSize <= 0)
+        {
+            return;
+        }
+        if (Count == maxSize)
+        {
+            items.RemoveFirst();
+        }
+        items.AddLast(item);
+    }
+
+    public T Pop()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty.");
+        }
+        var item = items.Last!.Value;
+        items.RemoveLast();
+        return item;
+    }
+
+    public int Count => items.Count;
+}
---
+++ 1.queue_stack_generic/LimitedSizeStack/ListModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class ListModel<TItem>
+{
+    public List<TItem> Items { get; }
+    public int UndoLimit;
+    private readonly LimitedSizeStack<ItemCommand> undoStack;
+
+    private class ItemCommand(ItemCommand.CommandType type, TItem item, int index)
+    {
+        public enum CommandType
+        {
+            Add,
+            Remove
+        }
+
+        public CommandType Type { get; } = type;
+        public TItem Item { get; } = item;
+        public int Index { get; } = index;
+    }
+
+    public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
+    {
+        undoStack = new LimitedSizeStack<ItemCommand>(undoLimit);
+    }
+
+    public ListModel(List<TItem> items, int undoLimit)
+    {
+        Items = items;
+        UndoLimit = undoLimit;
+        undoStack = new LimitedSizeStack<ItemCommand>(undoLimit);
+    }
+
+    public void AddItem(TItem item)
+    {
+        Items.Add(item);
+        undoStack.Push(new ItemCommand(ItemCommand.CommandType.Add, item, Items.Count - 1));
+    }
+
+    public void RemoveItem(int index)
+    {
+        if (index < 0 || index >= Items.Count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+        }
+        var removedItem = Items[index];
+        Items.RemoveAt(index);
+        undoStack.Push(new ItemCommand(ItemCommand.CommandType.Remove, removedItem, index));
+    }
+
+    public bool CanUndo()
+    {
+        return undoStack.Count > 0;
+    }
+
+    public void Undo()
+    {
+        if (!CanUndo())
+        {
+            throw new InvalidOperationException("No actions to undo.");
+        }
+
+        var command = undoStack.Pop();
+        switch (command.Type)
+        {
+            case ItemCommand.CommandType.Add:
+                Items.RemoveAt(command.Index);
+                break;
+            case ItemCommand.CommandType.Remove:
+                Items.Insert(command.Index, command.Item);
+                break;
+        }
+    }
+}
