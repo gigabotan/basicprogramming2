99ec9a8593e5cd8cb9e1a95b35ebf687e203151d
gigabotan <aaazat7@gmail.com>
2026-04-07 00:17:19 +0200

    feat: chatper 7 solution 1

--- 7.graphs/Dungeon/BfsTask.cs
+++ 7.graphs/Dungeon/BfsTask.cs
@@ -4,8 +4,50 @@ namespace Dungeon;
 
 public class BfsTask
 {
-	public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Chest[] chests)
-	{
-		yield break;
-	}
+    public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Chest[] chests)
+    {
+        if (map == null || chests == null)
+            yield break;
+
+        var remainingChests = new HashSet<Point>();
+        foreach (var chest in chests)
+        {
+            if (chest?.Location != null)
+                remainingChests.Add(chest.Location);
+        }
+
+        var visited = new HashSet<Point>();
+        var queue = new Queue<SinglyLinkedList<Point>>();
+
+        var startNode = new SinglyLinkedList<Point>(start);
+        queue.Enqueue(startNode);
+        visited.Add(start);
+
+        while (queue.Count > 0 && remainingChests.Count > 0)
+        {
+            var node = queue.Dequeue();
+            var current = node.Value;
+
+            if (remainingChests.Contains(current))
+            {
+                yield return node;
+                remainingChests.Remove(current);
+            }
+
+            foreach (var offset in Walker.PossibleDirections)
+            {
+                var next = current + offset;
+                if (!map.InBounds(next))
+                    continue;
+                if (visited.Contains(next))
+                    continue;
+                if (map.Dungeon[next.X, next.Y] == MapCell.Wall)
+                    continue;
+
+                visited.Add(next);
+                var nextNode = new SinglyLinkedList<Point>(next, node);
+                queue.Enqueue(nextNode);
+            }
+        }
+    }
 }
