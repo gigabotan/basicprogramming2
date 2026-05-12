c32533be121167f2cadc72ee475f82af2d0091ba
gigabotan <aaazat7@gmail.com>
2026-05-12 23:07:42 +0200

    feat: chapter 10 first and second tasks

--- 10.dynamic/Antiplagiarism/LevenshteinCalculator.cs
+++ 10.dynamic/Antiplagiarism/LevenshteinCalculator.cs
@@ -12,10 +12,40 @@ public class LevenshteinCalculator
 {
 	public List<ComparisonResult> CompareDocumentsPairwise(List<DocumentTokens> documents)
 	{
-		return new List<ComparisonResult> {
-			new(
-				documents[0], 
-				documents[1], 
-				TokenDistanceCalculator.GetTokenDistance(documents[0][0], documents[1][0]))};
+		var comparisons = new List<ComparisonResult>();
+		for (var first = 0; first < documents.Count; first++)
+		for (var second = first + 1; second < documents.Count; second++)
+			comparisons.Add(new ComparisonResult(
+				documents[first],
+				documents[second],
+				GetDistance(documents[first], documents[second])));
+		return comparisons;
+	}
+
+	private static double GetDistance(DocumentTokens first, DocumentTokens second)
+	{
+		var distances = new double[first.Count + 1, second.Count + 1];
+		for (var i = 0; i <= first.Count; i++)
+			distances[i, 0] = i;
+		for (var j = 0; j <= second.Count; j++)
+			distances[0, j] = j;
+
+		for (var i = 1; i <= first.Count; i++)
+		for (var j = 1; j <= second.Count; j++)
+		{
+			var deletion = distances[i - 1, j] + 1;
+			var insertion = distances[i, j - 1] + 1;
+			var replacement = distances[i - 1, j - 1]
+			                  + TokenDistanceCalculator.GetTokenDistance(first[i - 1], second[j - 1]);
+			distances[i, j] = Min(deletion, insertion, replacement);
+		}
+
+		return distances[first.Count, second.Count];
+	}
+
+	private static double Min(double first, double second, double third)
+	{
+		if (first <= second && first <= third) return first;
+		return second <= third ? second : third;
 	}
 }
--- 10.dynamic/Antiplagiarism/LongestCommonSubsequenceCalculator.cs
+++ 10.dynamic/Antiplagiarism/LongestCommonSubsequenceCalculator.cs
@@ -1,23 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Antiplagiarism;
 
 public static class LongestCommonSubsequenceCalculator
 {
-	public static List<string> Calculate(List<string> first, List<string> second)
+	public static List<string> Calculate(List<string> firstDocument, List<string> secondDocument)
 	{
-		var opt = CreateOptimizationTable(first, second);
-		return RestoreAnswer(opt, first, second);
+		var longestSubsequenceLengths = CreateOptimizationTable(firstDocument, secondDocument);
+		return RestoreAnswer(longestSubsequenceLengths, firstDocument, secondDocument);
 	}
 
-	private static int[,] CreateOptimizationTable(List<string> first, List<string> second)
+	private static int[,] CreateOptimizationTable(List<string> firstDocument, List<string> secondDocument)
 	{
-		throw new NotImplementedException();
+		var longestSubsequenceLengths = new int[firstDocument.Count + 1, secondDocument.Count + 1];
+		for (var firstLength = 1; firstLength <= firstDocument.Count; firstLength++)
+		for (var secondLength = 1; secondLength <= secondDocument.Count; secondLength++)
+			if (firstDocument[firstLength - 1] == secondDocument[secondLength - 1])
+				longestSubsequenceLengths[firstLength, secondLength]
+					= longestSubsequenceLengths[firstLength - 1, secondLength - 1] + 1;
+			else
+				longestSubsequenceLengths[firstLength, secondLength] = Max(
+					longestSubsequenceLengths[firstLength - 1, secondLength],
+					longestSubsequenceLengths[firstLength, secondLength - 1]);
+		return longestSubsequenceLengths;
 	}
 
-	private static List<string> RestoreAnswer(int[,] opt, List<string> first, List<string> second)
+	private static List<string> RestoreAnswer(
+		int[,] longestSubsequenceLengths,
+		List<string> firstDocument,
+		List<string> secondDocument)
 	{
-		throw new NotImplementedException();
+		var longestSubsequence = new List<string>();
+		var firstLength = firstDocument.Count;
+		var secondLength = secondDocument.Count;
+		while (firstLength > 0 && secondLength > 0)
+		{
+			if (firstDocument[firstLength - 1] == secondDocument[secondLength - 1])
+			{
+				longestSubsequence.Add(firstDocument[firstLength - 1]);
+				firstLength--;
+				secondLength--;
+			}
+			else if (longestSubsequenceLengths[firstLength - 1, secondLength]
+			         >= longestSubsequenceLengths[firstLength, secondLength - 1])
+				firstLength--;
+			else
+				secondLength--;
+		}
+
+		longestSubsequence.Reverse();
+		return longestSubsequence;
+	}
+
+	private static int Max(int firstValue, int secondValue)
+	{
+		return firstValue >= secondValue ? firstValue : secondValue;
 	}
 }
