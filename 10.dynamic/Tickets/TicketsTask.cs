7305c78cccc188b34309f7f3779cc48e857500f5
gigabotan <aaazat7@gmail.com>
2026-05-12 23:18:50 +0200

    feat: chapter 10 third solution

--- 10.dynamic/Tickets/TicketsTask.cs
+++ 10.dynamic/Tickets/TicketsTask.cs
@@ -1,1 +1,47 @@
-﻿namespace Tickets;
+﻿using System.Numerics;
+
+namespace Tickets;
+
+public static class TicketsTask
+{
+	public static BigInteger Solve(int halfLen, int totalSum)
+	{
+		if (totalSum % 2 != 0)
+			return BigInteger.Zero;
+
+		var halfSum = totalSum / 2;
+		if (halfSum < 0 || halfSum > 9 * halfLen)
+			return BigInteger.Zero;
+
+		var count = CountNumbersWithSum(halfLen, halfSum);
+		return count * count;
+	}
+
+	private static BigInteger CountNumbersWithSum(int length, int sum)
+	{
+		var counts = new BigInteger[sum + 1];
+		counts[0] = BigInteger.One;
+
+		for (var digits = 0; digits < length; digits++)
+			counts = AddDigit(counts);
+
+		return counts[sum];
+	}
+
+	private static BigInteger[] AddDigit(BigInteger[] counts)
+	{
+		var next = new BigInteger[counts.Length];
+		var window = BigInteger.Zero;
+
+		for (var sum = 0; sum < counts.Length; sum++)
+		{
+			window += counts[sum];
+			if (sum > 9)
+				window -= counts[sum - 10];
+
+			next[sum] = window;
+		}
+
+		return next;
+	}
+}
